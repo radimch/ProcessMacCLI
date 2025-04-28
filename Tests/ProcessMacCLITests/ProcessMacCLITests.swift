5b492a14952f1b001cb55b0e95149817fa471065
radimch <radimch@users.noreply.github.com>
2025-04-28 22:57:10 +0200

    Add test of ps aux

--- Tests/ProcessMacCLITests/ProcessMacCLITests.swift
+++ Tests/ProcessMacCLITests/ProcessMacCLITests.swift
@@ -7,14 +7,35 @@
 import XCTest
 @testable import ProcessMacCLI
 
+
 final class ProcessMacCLITests: XCTestCase {
-    func testShell() throws {
+    /**
+      Tests if running echo some-text in shell return the same some-text
+     */
+    func testShellEcho() throws {
         let outputString = "Hello, world!"
         let actual = shell("""
                 echo \(outputString) | tr -d "\n"§
                 """)
         let expected = outputString
 
+        XCTAssertEqual(actual, expected)
+    }
+    
+    /**
+      Test if ps aux returns eleven columns as expected
+      % ps aux | head -n1
+      USER               PID  %CPU %MEM      VSZ    RSS   TT  STAT STARTED      TIME COMMAND
+      % ps aux | head -n1 | wc -w
+      11
+      ( The final tr -d "\n"$ removes trailing new line )
+     */
+    func testShellPsAux() throws {
+        let expected = "11"
+        let actual = shell("""
+                 ps aux | head -n1 | wc -w | tr -d "\n"§
+            """)
+        
         XCTAssertEqual(actual, expected)
     }
 }
